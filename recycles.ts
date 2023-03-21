60146910b5d11396dc2950a4349a28a7292238a2
namanpreetsingh-d <88496682+namanpreetsingh-d@users.noreply.github.com>
2023-03-21 21:47:31 +0530

    Update recycles.ts

--- recycles.ts
+++ recycles.ts
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
- */
+ */ 
 
 import { Request, Response } from 'express'
 import { RecycleModel } from '../models/recycle'
